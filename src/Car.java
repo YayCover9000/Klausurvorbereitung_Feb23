714e64cd64a2157d639717c71b3d59f7ca2dd11c
YayCover9000 <yassuanf@googlemail.com>
2023-02-06 22:05:59 +0100

    added Eigenschaften von Attributen

---
+++ src/Car.java
@@ -0,0 +1,28 @@
+public class Car {
+    private String color; // Farbe des Autos
+    private String brand; // Marke des Autos
+    private int horsePower; // PS des Autos
+
+    public void drive() {
+        System.out.println("Das Auto fährt.");
+        public String getColor() {
+            return color;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+        public String getBrand() {
+            return brand;
+        }
+        public void setBrand(String brand) {
+            this.brand = brand;
+        }
+        public int getHorsePower() {
+            return horsePower;
+        }
+        public void setHorsePower(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+    }
+}
---
+++ src/CarProgram.java
@@ -0,0 +1,8 @@
+public class CarProgram {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.drive();
+        car1.setColor("Rot");
+        System.out.println(car1.getColor());
+    }
+}
